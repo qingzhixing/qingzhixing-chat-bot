23c98b388ff9a6a5425c4734597c4692adb28507
qingzhixing <1418390991@qq.com>
2023-01-21 00:43:28 +0800

    迁移前备份

--- src/main/java/org/qingzhixing/EventListeners/GroupPlainTextHandlers/AbstractGroupPlainTextHandler.java
+++ src/main/java/org/qingzhixing/EventListeners/GroupPlainTextHandlers/AbstractGroupPlainTextHandler.java
@@ -6,6 +6,28 @@ import net.mamoe.mirai.message.data.PlainText;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractGroupPlainTextHandler {
-    //true 为 中断事件处理，事件不再往后传播给其他Handler
-    public abstract boolean HandleGroupPlainText(@NotNull PlainText plainText, boolean atBot, @NotNull Member sender, @NotNull Group group);
+    protected PlainText plainText;
+    protected boolean atBot;
+    protected Member sender;
+    protected Group group;
+
+    public final void BindContext(@NotNull PlainText plainText, boolean atBot, @NotNull Member sender, @NotNull Group group) {
+        this.plainText = plainText;
+        this.atBot = atBot;
+        this.sender = sender;
+        this.group = group;
+    }
+
+    /*
+     * @return 返回 true 为 中断事件处理，事件不再往后传播给其他Handler
+     */
+    abstract public boolean Handle();
+
+    /*
+     * @return 返回 true 为 中断事件处理，事件不再往后传播给其他Handler
+     */
+    public final boolean BindContextAndHandle(@NotNull PlainText plainText, boolean atBot, @NotNull Member sender, @NotNull Group group) {
+        BindContext(plainText, atBot, sender, group);
+        return Handle();
+    }
 }
