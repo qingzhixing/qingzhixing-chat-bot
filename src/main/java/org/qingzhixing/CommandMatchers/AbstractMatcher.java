b49d6064499bb98c288a46a6829cde72ffe72f8f
qingzhixing <1418390991@qq.com>
2023-02-01 21:43:17 +0800

    AbstractMatcher.java

--- src/main/java/org/qingzhixing/CommandMatchers/AbstractMatcher.java
+++ src/main/java/org/qingzhixing/CommandMatchers/AbstractMatcher.java
@@ -1,43 +1,100 @@
 package org.qingzhixing.CommandMatchers;
 
+
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
 public abstract class AbstractMatcher {
+    private static final Logger logger = Logger.getLogger(AbstractMatcher.class);
     //TODO:Complete
-    protected String description;
+    private String description;
 
-    protected String commandName;
+    private String commandName;
 
-    protected MatchMode mode;
+    private MatchMode mode;
 
-    protected String originalText;
+    private String originalText;
 
-    public AbstractMatcher() {
+    public AbstractMatcher(@NotNull String originalText) {
         description = "";
         commandName = "";
         mode = MatchMode.DEFAULT;
+        this.originalText = originalText;
+    }
+
+    public AbstractMatcher(@NotNull String description, @NotNull String commandName, @NotNull MatchMode mode, @NotNull String originalText) {
+        this.description = description.trim();
+        this.commandName = commandName.trim();
+        this.mode = mode;
+        this.originalText = originalText.trim();
+    }
+
+    public String commandName() {
+        return commandName;
+    }
+
+    public MatchMode mode() {
+        return mode;
+    }
+
+    public String originalText() {
+        return originalText;
+    }
+
+    public String description() {
+        return description;
     }
 
     public abstract void Handle();
 
     //返回是否成功匹配
-    public boolean Match() {
+    public final boolean Match() {
         //空指令则不匹配
-        return !commandName.equals("");
-        //TODO:Complete
+        if (commandName.equals("")) return false;
+        switch (mode) {
+            case DEFAULT: {
+                var subString = '/' + commandName;
+                return originalText.startsWith(subString);
+            }
+            case CONTAINS: {
+                return originalText.contains(commandName);
+            }
+            default: {
+                logger.error("存在一个非法mode,进入了switch default中");
+                return false;
+            }
+        }
     }
 
     //返回是否成功匹配
-    public boolean MatchAndHandle() {
+    public final boolean MatchAndHandle() {
         if (!Match()) return false;
         Handle();
         return true;
     }
 
-    //返回指令描述
-    public final String description() {
-        return description;
+    public AbstractMatcher setDescription(@NotNull String description) {
+        this.description = description.trim();
+        return this;
     }
 
-    enum MatchMode {
+    public AbstractMatcher setCommandName(@NotNull String commandName) {
+        this.commandName = commandName.trim();
+        return this;
+    }
+
+    public AbstractMatcher setMode(@NotNull MatchMode mode) {
+        this.mode = mode;
+        return this;
+    }
+
+    public AbstractMatcher setOriginalText(@NotNull String originalText) {
+        this.originalText = originalText.trim();
+        return this;
+    }
+
+
+    public enum MatchMode {
         DEFAULT,        // 按 /指令名 的方式在文本最开始匹配
         CONTAINS        // 只要包含指令名都算匹配成功
     }
