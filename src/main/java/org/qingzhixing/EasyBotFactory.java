789bdd64fd83cea69d8022737c8528ce3f3667d5
qingzhixing <1418390991@qq.com>
2023-03-24 21:58:41 +0800

    need bug fixing

--- src/main/java/org/qingzhixing/EasyBotFactory.java
+++ src/main/java/org/qingzhixing/EasyBotFactory.java
@@ -17,7 +17,7 @@ public final class EasyBotFactory {
                 new BotConfiguration() {{
                     setHeartbeatStrategy(HeartbeatStrategy.REGISTER);
                     // pad支持多台设备同时登陆
-                    setProtocol(MiraiProtocol.IPAD);
+                    setProtocol(MiraiProtocol.ANDROID_WATCH);
                     // 缓存文件夹，最终为 workingDir 目录中的 cache 目录
                     setCacheDir(new File("cache"));
                     enableContactCache();
